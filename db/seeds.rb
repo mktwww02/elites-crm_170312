7026dd572723fe86b608c60a0114ba623739ae52
mika watanabe <watanabe-mika@av.avex.co.jp>
2017-03-14 11:08:40 +0000

    add 02

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |index|
+  Faker::Config.locale = :ja
+  family_name = Faker::Name.last_name
+  given_name = Faker::Name.first_name
+  
+  Faker::Config.locale = :en
+  email = Faker::Internet.email
+
+  Customer.create(
+    family_name: family_name,
+    given_name: given_name,
+    email: email
+  )
+end
